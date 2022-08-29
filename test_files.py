9327a3eb596e2d9f246183c7bb64e7326c466af4
ivstarkov <104798391+ivstarkov@users.noreply.github.com>
2022-08-29 15:08:10 +0400

    name-based delet worked

--- test_files.py
+++ test_files.py
@@ -2,11 +2,20 @@ import os
 import re
 
 
-for name in os.listdir(r"D:\Test_folder"):
-    c_name = re.search(r"(^\w+)(?: ?\(\d\)| - Copy.*)\.\w+$", name, re.IGNORECASE)
-    if c_name:
+dir = r"D:\Test_folder"
+f_list = os.listdir(dir)
+for i, name in enumerate(f_list):
+    s_name = re.search(r"(^\w+)(?: ?\(\d+\)| - Copy.*)(\.\w+)$", name, re.IGNORECASE)
+    
+    if s_name:
+        s_name = s_name.group(1) + s_name.group(2)
+        
+        # Compare stripped name with next 5 names
+        for j in range(1, 6):
+            try:
+                if s_name == f_list[i + j]:
+                    # TODO: size and header comparison 
+                    os.remove(dir + "\\" + name)
+            except:
+                break
 
-        # Compare clear name with next 5 names
-        for i in range(5):
-
-        print(c_name.group(1))
