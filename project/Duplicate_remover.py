3a7af2a38016be834515661334c45f4765bf3cab
ivstarkov <104798391+ivstarkov@users.noreply.github.com>
2022-08-29 20:10:02 +0400

    reformat

--- project/Duplicate_remover.py
+++ project/Duplicate_remover.py
@@ -8,21 +8,21 @@ class MainWindow(QDialog):
 
     def __init__(self):
         super(MainWindow, self).__init__()
-        loadUi("test.ui", self)
+        loadUi(r"project\test.ui", self)
         self.browse.clicked.connect(self.browsedir)
 
     def browsedir(self):
-        f_name = QFileDialog.getExistingDirectory(self, "Chose Directory", r"D:\Games", QFileDialog.Option.DontUseNativeDialog) 
-        print(f_name)
-        self.file_name.setText(f_name)
+        dir = QFileDialog.getExistingDirectory(self, "Chose Directory", r"D:\Games", QFileDialog.Option.DontUseNativeDialog) 
+        print(dir)
+        self.dir_name.setText(dir)
 
 
 app = QApplication(sys.argv)
 main_window = MainWindow()
 widget = QtWidgets.QStackedWidget()
 widget.addWidget(main_window)
-#widget.setFixedWidth(400)
-#widget.setFixedHeight(200)
+widget.setFixedWidth(400)
+widget.setFixedHeight(300)
 
 widget.show()
 
